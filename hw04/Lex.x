2f4853b4a3c072ea6e6ce20de40245a439233a7c
Худяков Юрий <catchitoo@mail.ru>
2020-09-30 23:41:16 +0300

    Add parser

---
+++ hw04/Lex.x
@@ -0,0 +1,35 @@
+{
+module Lex where
+}
+
+%wrapper "posn"
+
+$digit = 0-9			-- digits
+$alpha = [a-zA-Z\_]		-- alphabetic characters
+
+tokens :-
+
+  $white+				        ;
+  \:\-					        { \p _ -> Turnstile p }
+  \,                            { \p _ -> Comma p }
+  \;                            { \p _ -> Semicolon p }                     
+  \.					        { \p _ -> Dot p }
+  \(                            { \p _ -> Lb p }
+  \)                            { \p _ -> Rb p }
+  $alpha [$alpha $digit]*		{ \p s -> Word s p }
+  .                             { \p s -> Error s p }
+{
+-- Each action has type :: String -> Token
+
+-- The token type:
+data Token =
+    Turnstile    AlexPosn |
+    Comma        AlexPosn |
+    Semicolon    AlexPosn |
+    Dot          AlexPosn |
+    Lb           AlexPosn |
+    Rb           AlexPosn |
+    Word  String AlexPosn |
+    Error String AlexPosn
+	deriving (Eq,Show)
+}
---
+++ hw04/main.hs
@@ -0,0 +1,77 @@
+module Main where
+import Data.Maybe
+import Text.Printf
+import Lex
+
+-- G -> L | LG
+-- L -> W :- B. | W.
+-- B -> K;B | K
+-- K -> T,K | T
+-- T -> W | (B)
+-- W # Word from lexer
+
+getLexemError :: [Token] -> Maybe Token
+getLexemError []             = Nothing
+getLexemError (Error w p:ts) = Just $ Error w p
+getLexemError (_:ts)         = getLexemError ts
+
+parseG :: [Token] -> (Bool, [Token])
+parseG [] = (True, [])
+parseG ts = case parseL ts of
+                 (True, ts') -> parseG ts'
+                 e           -> e
+
+parseL :: [Token] -> (Bool, [Token])
+parseL [] = (True, [])
+parseL (Word _ _:Dot _:t) = parseG t
+parseL (Word _ _:Turnstile _:ts) = case parseB ts of
+                                        (True, Dot _:ts') -> (True, ts')
+                                        (True, ts')         -> (False, ts')
+                                        e                   -> e
+parseL t = (False, t)
+
+parseB :: [Token] -> (Bool, [Token])
+parseB [] = (False, [])
+parseB ts = case parseK ts of
+                 (True, Semicolon _:ts') -> parseB ts'
+                 (True, ts')                -> (True, ts')
+                 e                          -> e
+
+parseK :: [Token] -> (Bool, [Token])
+parseK [] = (False, [])
+parseK ts = case parseT ts of
+                 (True, Comma _:ts') -> parseK ts'
+                 (True, ts')            -> (True, ts')
+                 e                      -> e
+
+parseT :: [Token] -> (Bool, [Token])
+parseT (Word _ _:ts) = (True, ts)
+parseT (Lb _:ts)     = case parseB ts of
+                            (True, Rb _:ts') -> (True, ts')
+                            (True, ts')       -> (False, ts')
+                            e                 -> e
+parseT ts            = (False, ts)                           
+
+parseError :: Token -> String
+parseError e = printf "Syntax error at word \"%s\": line %d, colon %d"  w l c
+               where (w, (l, c)) = case e of
+                                      Turnstile p -> (":-", getPos p)
+                                      Comma     p -> ("," , getPos p)
+                                      Semicolon p -> (";" , getPos p)
+                                      Dot       p -> ("." , getPos p)
+                                      Lb        p -> ("(" , getPos p)
+                                      Rb        p -> (")" , getPos p)
+                                      Word    w p -> (w   , getPos p)
+                                      Error   w p -> (w   , getPos p)
+                                   where getPos (AlexPn _ l c) = (l, c)
+
+main = do
+        s <- getContents
+        putStrLn $ let tokens = alexScanTokens s in
+                   case getLexemError tokens of
+                        Just e  -> parseError e
+                        Nothing -> case parseG tokens of
+                                        (True, []) -> "Correct syntax"
+                                        (False, ts) -> printf "Incorrect syntax. %s" e
+                                                        where e = case ts of [] -> "Tokens list is empty. Perhaps some lexem is missing"
+                                                                             (t:_) -> parseError t
