dfe7036182b3c3f271a22893fdb9c9bdbe4290cb
Худяков Юрий <catchitoo@mail.ru>
2020-10-06 17:23:31 +0300

    Add command line options to hw_05

--- hw05/main.hs
+++ hw05/main.hs
@@ -1,6 +1,19 @@
 module Main where
 import Lex
 import Parse
+import System.Environment
+import System.IO
 
-main = do s <- getContents
-          print $ parse $ alexScanTokens s
+main' s = show $ parse $ alexScanTokens s
+
+main = do
+        args <- getArgs
+        case length args of
+            0 -> putStr $ unlines ["Usage:", "Read from file: ./main -f filename", "Read from stdin: ./main -s"]
+            1 -> case head args of
+                    "-s" -> getContents >>= putStrLn . main' 
+                    _    -> putStrLn "Wrong arguments"
+            2 -> case head args of
+                    "-f" -> openFile (args !! 1) ReadMode >>= hGetContents >>= putStrLn . main'
+                    _    -> putStrLn "Wrong arguments"
+            _ -> putStrLn "Wrong arguments"
